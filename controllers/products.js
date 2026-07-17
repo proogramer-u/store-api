40ae1a897724e3baee6c07e0fb8041129529e70a
proogramer-u <cok.iswara2007@gmail.com>
2026-07-17 19:28:49 +0700

    feat: add product filtering by featured and company

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,23 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req,res) => {
-    const products = await Product.find({})
-    res.status(200).json({ products })
+    const products = await Product.find({
+        
+    })
+    res.status(200).json({ products,nbHits: products.length })
 }
 const getAllProducts = async (req,res) => {
-    res.status(200).json({msg:'products testing routes'})
+    const { featured } = req.query
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === 'true'? true : false
+    }
+    
+    console.log(queryObject)
+
+    const products = await Product.find(queryObject);
+    res.status(200).json({ products,nbHits: products.length })
 }
 
 module.exports = {
